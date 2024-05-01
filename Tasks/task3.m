50124428e27b96fc9a7d3c0c2c58a4c5d401595d
Yann Debray <128002745+yanndebray@users.noreply.github.com>
2024-05-01 14:25:23 +0000

    Add new task

--- Tasks/task3.m
+++ Tasks/task3.m
@@ -1,10 +1,3 @@
-%% Basic example
-t = [0:0.1:2*pi];
-y = sin(t);
-plot(t,y);
-ax = gca;
-exportgraphics(ax, "Data/sine.png")
-
 %% Weather example
 jason = webread("https://api.weather.gov/gridpoints/BOX/62,85/forecast/hourly");
 
@@ -13,6 +6,7 @@ T = struct2table(jason.properties.periods);
 time = datetime(T.startTime,"InputFormat","uuuu-MM-dd'T'HH:mm:ssssZZZZZ","TimeZone","America/New_York");
 temperature = T.temperature;
 TT = timetable(time,temperature);
+writetimetable(TT,"Data/weather.csv");
 
 % Plot data over time
 plot(TT.time,TT.temperature);
